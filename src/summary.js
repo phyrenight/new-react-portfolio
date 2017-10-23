03712977b60acb86d2de969723f26d4ae3ae6b8a
Marc Preston <phyrenight@gmail.com>
2017-10-22 20:14:10 -0400

    feat: summary section

---
+++ src/summary.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Summary extends Component{
+	render() {
+	  return (
+      <section className="Summary">
+        <h2>About Me</h2>
+        <p>Self taught Python Web developer. Currently taking the Udacity React nanodegree program.
+        </p>
+      </section>
+    )
+	}
+}
+
+export default Summary;
