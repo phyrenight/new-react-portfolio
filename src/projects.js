8a89ced9a05b41ddbfe104e4199499eabee1bfb2
Marc Preston <phyrenight@gmail.com>
2017-11-08 03:32:14 -0500

    add projects

--- src/projects.js
+++ src/projects.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 
 class Projects extends Component{
+  constructor(props){
+    super(props);
+    this.state = {projects: [
+      {name: "phoneBook",url: "", github: "",skills:["Python", "Flask", "SQlalchemy"]},
+      {name: "",url: "", github: "",skills:[]},
+      {name: "",url: "", github: "",skills:[]},
+      {name: "",url: "", github: "",skills:[]},
+      {name: "",url: "", github: "",skills:[]},
+      {name: "",url: "", github: "",skills:[]}
+    ]}
+}
 	render(){
     return(
       <section className="Project">
         <h2>Projects</h2>
-        <div>
+        <div className="row">
           <div>
+            <img src="" alt="" />
+            <p>title</p>
           </div>
           <div>
+            <img src="" alt="" />
+            <p>title</p>
           </div>
           <div>
+            <img src="" alt="" />
+            <p>title</p>
           </div>
         </div>
-        <div>
+        <div className="row">
           <div>
+            <img src="" alt="" />
+            <p>title</p>
           </div>
           <div>
+            <img src="" alt="" />
+            <p>title</p>
           </div>
           <div>
+            <img src="" alt="" />
+            <p>title</p>
           </div>
         </div>
       </section>
